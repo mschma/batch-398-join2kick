3390f8fdd66a07bdb0130c74c43ca553fcd07ad2
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-05-26 15:25:09 +0200

    setup routes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'pitches/index'
+  get 'pitches/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :pitches, only: [:index, :show]
 end
