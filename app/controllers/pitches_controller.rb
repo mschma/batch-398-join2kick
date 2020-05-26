e026f0a406da0389b7bc64a8efcdf967f8f9308b
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-05-26 16:50:14 +0200

    added search at home and implement controller

--- app/controllers/pitches_controller.rb
+++ app/controllers/pitches_controller.rb
@@ -1,7 +1,14 @@
 class PitchesController < ApplicationController
   def index
+    @pitches = Pitch.all
+    @search = params["search"]
+    if @search.present?
+      @name = @search["name"]
+      @pitches = Pitch.where(name: @name)
+    end
   end
 
   def show
+    @pitch = Pitch.find(params[:id])
   end
 end
