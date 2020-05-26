f25cb8f9b034a8d7c8017b95c6e7ec2ac4a639cb
Josue Sirker <jsirkerj@gmail.com>
2020-05-26 18:04:50 +0200

    pundit wannabe

--- app/controllers/pitches_controller.rb
+++ app/controllers/pitches_controller.rb
@@ -1,7 +1,11 @@
 class PitchesController < ApplicationController
   def index
+    @pitches = Pitch.all
   end
 
   def show
+    # skip_authorization
+    @pitch = Pitch.find(params[:id])
+    authorize @pitch
   end
 end
