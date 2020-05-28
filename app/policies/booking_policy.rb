f4f0ce110fdcfe7abfcd12fe9d08b2cd7b23ae7a
Martin Schmalz <msacc10001@gmail.com>
2020-05-28 12:52:19 +0200

    book-and-save-a-pitch

---
+++ app/policies/booking_policy.rb
@@ -0,0 +1,20 @@
+class BookingPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+      # scope.where(user: user)
+    end
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    true
+  end
+end
